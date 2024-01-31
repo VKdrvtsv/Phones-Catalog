ec033d1cb282c44d6147dff6ded96eecba3bebdd
VKdrvtsv <valery.volikova1@gmail.com>
2024-01-31 20:37:14 +0200

    Phone Catalog

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# React TypeScript Starter Pack
+# Phone Catalog
 
-To use this template click `Use this template`
+This is a site-catalod for a hardware store. You can see some goods card on a main page, also you can open the page with details of a choosen good. Now we have only phones, but it's not a problem to add other categories. Also you can choose favoriets or do an order.
 
-### Available Scripts
+This page was created using React, TypeScrips and SASS preprocessor technologies. Also BEM methodology was used during creation this project. Besides it was implemented some libraries like Swiper and Slick carousel.
 
-`Deploy` - available to deploy your application to gh-pages
+[DEMO LINK](https://VKdrvtsv.github.io/Phone-Catalog/)
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+The mockup (https://www.figma.com/file/uEetgWenSRxk9jgiym6Yzp/Phone-catalog-redesign?node-id=1%3A2)
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,70 @@
-import React from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
+
 import './App.scss';
 
-interface Props {
-  onClick: () => void;
-}
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { PageContext } from './utils/GlobalContext';
+import { getProducts } from './api';
+import { Loader } from './components/Loader';
 
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+export const App = () => {
+  const {
+    setProducts,
+    setError,
+    setTotalCount,
+    products,
+    cardList,
+  } = useContext(PageContext);
+  const [isLoading, setIsLoading] = useState(false);
 
-export const App: React.FC = () => {
+  useEffect(() => {
+    setIsLoading(true);
+
+    getProducts()
+      .then(setProducts)
+      .catch(() => setError(true))
+      .finally(() => setIsLoading(false));
+  }, [setError, setIsLoading, setProducts]);
+
+  useEffect(() => {
+    const productsOnCart = products.filter(p => cardList.includes(p.id));
+
+    const initialTotalCount = productsOnCart.map(el => {
+      const count = localStorage.getItem(el.id);
+
+      if (count === null) {
+        return {
+          id: el.id,
+          price: el.price,
+          count: 1,
+        };
+      }
+
+      return {
+        id: el.id,
+        price: el.price,
+        count: JSON.parse(count),
+      };
+    });
+
+    setTotalCount(initialTotalCount);
+  }, [cardList, products, setTotalCount]);
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+    <div className="App">
+      {isLoading
+        ? <Loader />
+        : (
+          <>
+            <Header />
+            <div className="root">
+              <Outlet />
+            </div>
+            <Footer />
+          </>
+        )}
     </div>
   );
 };
